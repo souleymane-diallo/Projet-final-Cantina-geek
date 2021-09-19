3ae539914942e942e5950e4d8500613d3c4757b4
John Doe <johndoe@example.com>
2021-09-20 00:11:17 +0200

    Edited a recipe

---
+++ src/components/FormEdit.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import {
+    Box, FormControl, FormLabel, Input,
+    Stack,
+    Button,
+    Textarea,
+    Flex, Select
+  } from '@chakra-ui/react';
+
+function FormEdit({handleForm, submitRecipe, addChamps, removeFormFields, onValidateForm}) {
+  return (
+    <Box
+      rounded={'lg'}
+      boxShadow={'lg'}
+      p={10}
+      mt={12}
+    >
+      {submitRecipe && (
+        <FormControl isRequired onChange={handleForm}>
+              <FormLabel>Titre</FormLabel>
+                <Input type="text" value={submitRecipe.titre} id="titre" />
+              <FormLabel>Description</FormLabel>
+              <Textarea id="description" defaultValue={submitRecipe.description} />
+            <Stack spacing={4} mt={4}>
+              <FormLabel>Personne</FormLabel>
+              <Input type="number" id="personnes" value={submitRecipe.personnes} />
+              <FormLabel>Niveau</FormLabel>
+              <Select id="niveau" value={submitRecipe.niveau}>
+                <option value="padawan">padawan</option>
+                <option value="jedi">jedi</option>
+                <option value="maitre">maitre</option>
+              </Select>
+              <FormLabel>Temps préparation</FormLabel>
+              <Input type="number" id="tempsPreparation" value={submitRecipe.tempsPreparation} />
+          </Stack>
+            <FormLabel>Image</FormLabel>
+              <Input type="text" />
+              <FormControl id="ingredients">
+              <FormLabel>Ingredients</FormLabel>
+              {submitRecipe.ingredients && submitRecipe.ingredients.map((value, index) => (
+                <Stack direction="row" spacing={4} mt={4} key={index} >
+                  <Input type="text" value={value[0]} id="quantite" onChange={(e) => handleForm(e, value, index)} />
+                  <Input type="text" value={value[1]} id="ingredient" onChange={(e) => handleForm(e, value, index)}  />
+                  <Button 
+                    bg={'red.400'} 
+                    color={'white'}
+                    _hover={{ bg: 'red.500'}}
+                    onClick={() => removeFormFields('ingrédient')}
+                  >
+                    X
+                  </Button>
+                </Stack>
+              ))}
+            </FormControl>
+            <Button
+              my={3}
+              bg={'blue.400'}
+              _hover={{ bg: 'blue.500'}} 
+              color={'white'}
+              onClick={() => addChamps('ingrédient')}
+            >
+            Ajouter un ingrédient</Button>
+            <FormControl id="etapes">
+              <FormLabel>Etages</FormLabel>
+                {submitRecipe.etapes && submitRecipe.etapes.map((value, index) => (
+                  <Stack direction="row"  mt={2} key={index}>
+                    <Textarea id="etape" value={value} onChange={(e) => handleForm(e, value, index)} />
+                      <Button bg={'red.400'} 
+                        _hover={{ bg: 'red.500'}}
+                        color={'white'}
+                        onClick={() => removeFormFields('étape')}
+                        >
+                          X
+                        </Button>
+                    </Stack>
+                ))}
+            </FormControl>
+            <Button
+              my={3}
+              bg={'blue.400'} 
+              color={'white'}
+              _hover={{ bg: 'blue.500'}}
+              onClick={() => addChamps('étape')}
+            >
+            Ajouter une étape</Button>
+          <Flex justifyContent="center">
+            <Button
+              mt={4}
+              onClick={onValidateForm}
+              justifyContent="center"
+              bg={'blue.400'}
+              color={'white'}
+              _hover={{ bg: 'blue.500'}}>
+              {submitRecipe.id ? 'Modifier une Recette' : 'Ajouter une recette' }
+            </Button>
+          </Flex>
+      </FormControl>
+      )}   
+    </Box>
+  )
+}
+
+export default FormEdit
--- src/pages/EditRecipe.jsx
+++ src/pages/EditRecipe.jsx
@@ -1,16 +1,116 @@
 import React, { useState, useEffect } from 'react';
-
+import { useParams, useHistory } from "react-router-dom";
 import {
     Flex,
     Stack,
     Heading,
   } from '@chakra-ui/react';
-//import { useParams } from 'react-router-dom';
 import FormEdit from '../components/FormEdit';
 
-
 const EditRecipe = () => {
-    
+    const params = useParams();
+    const id = params.id; 
+    const history = useHistory();
+    const [submitRecipe, setSubmitRecipe] = useState(null);
+
+    useEffect(() => {
+        fetch(`http://localhost:9000/api/recipe/${id}`)
+          .then((res) => res.json())
+          .then((recipes) => {
+            setSubmitRecipe(recipes);
+            console.log("de",recipes);
+          });
+    },[id]);
+
+    const handleForm = (e, index = null, value = null) => {
+        if (e.target.id === "personnes" || e.target.id === "tempsPreparation") {
+          setSubmitRecipe({
+            ...submitRecipe,
+            [e.target.id]: Number(e.target.value),
+          });
+        } else if (e.target.id === "quantite" && index != null) {
+          const newValue = submitRecipe;
+          newValue.ingredients[index][0] = e.target.value;
+          setSubmitRecipe({ newValue });
+        } else if (e.target.id === "ingredient" && index != null) {
+          const newValue = submitRecipe;
+          newValue.ingredients[index][1] = e.target.value;
+          setSubmitRecipe({ newValue });
+        } else if (e.target.id === "etape" && index != null) {
+          const newValue = submitRecipe;
+          newValue.etapes[index] = e.target.value;
+          setSubmitRecipe({ newValue });
+        } 
+        else {
+          setSubmitRecipe({
+            ...submitRecipe,
+            [e.target.id]: e.target.value,
+          });
+        }
+    };
+
+    const addChamps = (option) => {
+        if (option === "ingrédient") {
+          const recipe = submitRecipe.ingredients;
+        recipe.push(["", ""]);
+        setSubmitRecipe({
+          ...submitRecipe,
+          [submitRecipe.ingredients]: recipe,
+        });
+        } else if (option === "étape") {
+          const recipe = submitRecipe.etapes;
+        recipe.push("");
+        setSubmitRecipe({
+          ...submitRecipe,
+          [submitRecipe.etapes]: recipe,
+        });
+        }
+    };
+
+    const removeFormFields = (option, i) => {
+        if (option === "ingrédient") {
+          const recipe = submitRecipe.ingredients;
+        recipe.splice(i, 1);
+        setSubmitRecipe({
+          ...submitRecipe,
+          [submitRecipe.ingredients]: recipe,
+        });
+        } else if (option === "étape") {
+          const recipe = submitRecipe.etapes;
+        recipe.splice(i, 1);
+        setSubmitRecipe({
+          ...submitRecipe,
+          [submitRecipe.etapes]: recipe,
+        });
+        }
+      }
+
+    function onValidateForm() {
+    const requestOptions = {
+        method: "PUT",
+        headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(submitRecipe),
+    };
+    fetch(`http://localhost:9000/api/recipe/${id}`, requestOptions)
+        .then((res) => res.json())
+        .then(
+        (result) => {
+            setSubmitRecipe(result);
+            if(result.message) {
+                history.push('/')
+                alert(result.message)
+            }
+        },
+        (error) => {
+        //   setError(error);
+        console.log(error);
+        }
+        );
+    }
+
   return (
       <Flex
           minH={'100vh'}
@@ -20,7 +120,13 @@ const EditRecipe = () => {
               <Stack align={'center'}>
               <Heading fontSize={'4xl'}>Modifier une recette</Heading>
               </Stack>
-              <FormEdit />
+              <FormEdit 
+                  handleForm={handleForm} 
+                  addChamps={addChamps}
+                  onValidateForm={onValidateForm}
+                  removeFormFields={removeFormFields}
+                  submitRecipe={submitRecipe}
+              />
           </Stack>
       </Flex>
   )
