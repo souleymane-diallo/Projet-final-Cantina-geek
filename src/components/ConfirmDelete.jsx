c326920a653005ef760d796a4d7a50b7784491e1
John Doe <johndoe@example.com>
2021-09-20 00:12:40 +0200

    Create ConfirmDelete.jsx

---
+++ src/components/ConfirmDelete.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    Button,
+    AlertDialogOverlay,
+  } from "@chakra-ui/react"
+  import {AiFillDelete} from 'react-icons/ai'
+
+const ConfirmDelete = ({onDeleteRecipe, recipe}) => {
+    const [isOpen, setIsOpen] = React.useState(false)
+    const onClose = () => setIsOpen(false)
+    const cancelRef = React.useRef()
+    const deleteRef = React.useRef();
+    console.log(deleteRef.current);
+    return (
+        <>
+        <Button leftIcon={<AiFillDelete />} 
+          colorScheme="red" variant="outline" onClick={() => setIsOpen(true)}>
+          Delete
+        </Button>
+  
+        <AlertDialog
+          isOpen={isOpen}
+          leastDestructiveRef={cancelRef}
+          onClose={onClose}
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                Delete Customer
+              </AlertDialogHeader>
+              <AlertDialogBody>
+                Are you sure? You can't undo this action afterwards.
+              </AlertDialogBody>
+  
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  Cancel
+                </Button>
+                <Button colorScheme="red" ref={deleteRef} onClick={() =>{
+                    if(deleteRef.current) {
+                        onDeleteRecipe()
+                    }
+                } } ml={3}>
+                  Delete
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </>
+    )
+}
+
+export default ConfirmDelete
